d63e56090773599e05407f01e1bd23148ee164f9
Nate Choe <natechoe9@gmail.com>
2022-06-13 22:04:44 -0500

    Actually fixed the bug

--- run-tor.sh
+++ run-tor.sh
@@ -30,7 +30,7 @@ fi
 
 echo "SocksPort 0" >> $RELAY_CONFIG
 echo "Log notice	stdout" >> $RELAY_CONFIG
-echo "DataDirectory /var/lib/tor" >> $RELAY_CONFIG
+echo "DataDirectory /var/lib/tor/data" >> $RELAY_CONFIG
 echo "User	tor" >> $RELAY_CONFIG
 
 if [ -z "$RELAY_BANDWIDTH" ] ; then
