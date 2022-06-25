87d726c18361c20ad8d74349d4f2e288857b50c6
Nate Choe <nate@natechoe.dev>
2022-06-25 05:35:09 -0500

    Added nyx

--- artifacts/install-tor.sh
+++ artifacts/install-tor.sh
@@ -8,4 +8,4 @@ echo "deb     [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://de
 echo "deb-src [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org $DIST main" >> /etc/apt/sources.list.d/tor.list
 wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | tee /usr/share/keyrings/tor-archive-keyring.gpg >/dev/null
 apt-get update
-apt-get install -y tor
+apt-get install -y tor nyx
--- artifacts/run-tor.sh
+++ artifacts/run-tor.sh
@@ -7,36 +7,37 @@ rm $RELAY_CONFIG
 if [ -z "$RELAY_NICKNAME" ] ; then
 	echo "No relay nickname set!"
 else
-	echo "Nickname	$RELAY_NICKNAME" >> $RELAY_CONFIG
+	echo "Nickname $RELAY_NICKNAME" >> $RELAY_CONFIG
 fi
 
 if [ -z "$RELAY_CONTACT" ] ; then
 	echo "No relay contact set!"
 else
-	echo "ContactInfo	$RELAY_CONTACT" >> $RELAY_CONFIG
+	echo "ContactInfo $RELAY_CONTACT" >> $RELAY_CONFIG
 fi
 
 if [ -z "$RELAY_PORT" ] ; then
-	echo "ORPort	443" >> $RELAY_CONFIG
+	echo "ORPort 443" >> $RELAY_CONFIG
 else
-	echo "ORPort	$RELAY_PORT" >> $RELAY_CONFIG
+	echo "ORPort $RELAY_PORT" >> $RELAY_CONFIG
 fi
 
 if [ -z "$RELAY_EXIT" ] || [ "$RELAY_EXIT" -eq 0 ] ; then
-	echo "ExitRelay	0" >> $RELAY_CONFIG
+	echo "ExitRelay 0" >> $RELAY_CONFIG
 else
-	echo "ExitRelay	1" >> $RELAY_CONFIG
+	echo "ExitRelay 1" >> $RELAY_CONFIG
 fi
 
 echo "SocksPort 0" >> $RELAY_CONFIG
-echo "Log notice	stdout" >> $RELAY_CONFIG
+echo "Log notice stdout" >> $RELAY_CONFIG
 echo "DataDirectory /var/lib/tor/data" >> $RELAY_CONFIG
+echo "ControlPort 9051" >> $RELAY_CONFIG
 
 if [ -z "$RELAY_BANDWIDTH" ] ; then
 	echo "Relay bandwidth not set!"
 else
-	echo "BandwidthRate	$RELAY_BANDWIDTH" >> $RELAY_CONFIG
-	echo "BandwidthBurst	$RELAY_BANDWIDTH" >> $RELAY_CONFIG
+	echo "BandwidthRate $RELAY_BANDWIDTH" >> $RELAY_CONFIG
+	echo "BandwidthBurst $RELAY_BANDWIDTH" >> $RELAY_CONFIG
 fi
 
 tor
